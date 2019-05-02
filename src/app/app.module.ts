74019cd45927c57b6a0f8ab9d60939c681f9b8b4
573f <codesmith@me.com>
2019-05-02 13:13:08 -0600

    Basic ngrx infrastructure in place. Checkpoint #1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { initialState, reducers } from './app.state';
+
 import { AppRoutes } from './app.routing';
 import { AppComponent } from './app.component';
 import { BananaComponent } from './banana/banana.component';
@@ -15,6 +19,10 @@ import { BananaComponent } from './banana/banana.component';
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
+    StoreModule.forRoot(reducers, {initialState}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    }),
     RouterModule.forRoot(AppRoutes)
   ],
   declarations: [ 
